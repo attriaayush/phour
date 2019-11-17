59a998124346d8a828b603fcc4f59156454b5c0f
LilUziCert <attriaayush@yahoo.com>
2019-11-17 18:19:02 +0000

    main func for REPL controller

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import "os/user"
+
+import "fmt"
+
+import "phour/repl"
+
+import "os"
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s!. This is Phour the programming language!\n", user.Username)
+	fmt.Printf("Write this shiny REPL out\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
