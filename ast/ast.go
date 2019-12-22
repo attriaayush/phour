f2473f55fc89eaeb2e2aa8a99a35d114843aa275
LilUziCert <attriaayush@yahoo.com>
2019-12-22 18:06:33 +0000

    adding String() for debugging parser

--- ast/ast.go
+++ ast/ast.go
@@ -2,10 +2,13 @@ package ast
 
 import "phour/token"
 
+import "bytes"
+
 // Node represents each node for our AST which will get
 // represented by its's token literal
 type Node interface {
 	TokenLiteral() string
+	String() string // print AST nodes for debugging
 }
 
 // Statement .. when the parser comes across any statement
@@ -31,6 +34,16 @@ func (p *Program) TokenLiteral() string {
 		return p.Statements[0].TokenLiteral()
 	}
 	return ""
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
 }
 
 // LetStatement ..
@@ -59,10 +72,63 @@ func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 // ReturnStatement ..
 type ReturnStatement struct {
 	Token token.Token
-	Value string
+	Value Expression
 }
 
 func (r *ReturnStatement) statementNode() {}
 
 // TokenLiteral ..
 func (r *ReturnStatement) TokenLiteral() string { return r.Token.Literal }
+
+// ExpressionStatement struct contains Token which
+// every single Node has Expression which will hold
+// the expression
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+// TokenLiteral ..
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (r *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(r.TokenLiteral() + " ")
+
+	if r.Value != nil {
+		out.WriteString(r.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
+
+func (i *Identifier) String() string { return i.Value }
