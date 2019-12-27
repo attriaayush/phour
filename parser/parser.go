360242136aa651718a7569c0b5bc3617f4681e59
LilUziCert <attriaayush@yahoo.com>
2019-12-27 14:25:42 +0000

    grouped expressions to have higher precedence

--- parser/parser.go
+++ parser/parser.go
@@ -75,6 +75,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
 
+	p.registerPrefix(token.LPAREN, p.parseGroupedExpressions)
+
 	return p
 }
 
@@ -283,4 +285,16 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 
 func (p *Parser) parseBoolean() ast.Expression {
 	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
+}
+
+func (p *Parser) parseGroupedExpressions() ast.Expression {
+	p.nextToken()
+
+	exp := p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return exp
 }
