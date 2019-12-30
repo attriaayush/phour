013f059b8e6fe10e685adbeb9ec46b64db681866
LilUziCert <attriaayush@yahoo.com>
2019-12-30 11:40:26 +0000

    integrating parser to the repl

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"phour/lexer"
-	"phour/token"
+	"phour/parser"
 )
 
 // PROMPT ...
@@ -15,7 +15,6 @@ const PROMPT = ">>"
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
-	// forever loop
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -25,9 +24,23 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t+"+msg+"\n")
 	}
 }
