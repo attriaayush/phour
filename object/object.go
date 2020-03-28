199c2806b0d02294226e4712807d26241ec0c462
LilUziCert <attriaayush@yahoo.com>
2020-03-28 13:03:25 +0000

    hash map environment to hold the state

--- object/object.go
+++ object/object.go
@@ -10,6 +10,12 @@ const (
 	ERROR_OBJ        = "ERROR"
 )
 
+// NewEnvironment ..
+func NewEnvironment() *Environment {
+	s := make(map[string]Object)
+	return &Environment{store: s}
+}
+
 // ObjectType . .
 type ObjectType string
 
@@ -44,6 +50,11 @@ type Error struct {
 	Message string
 }
 
+// Environment struct
+type Environment struct {
+	store map[string]Object
+}
+
 // Type for Integer
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 
@@ -73,3 +84,15 @@ func (e *Error) Type() ObjectType { return ERROR_OBJ }
 
 // Inspect for Error
 func (e *Error) Inspect() string { return "ERROR: " + e.Message }
+
+// Get for Environment Getter
+func (e *Environment) Get(name string) (Object, bool) {
+	obj, ok := e.store[name]
+	return obj, ok
+}
+
+// Set for Environment Setter
+func (e *Environment) Set(name string, val Object) Object {
+	e.store[name] = val
+	return val
+}
