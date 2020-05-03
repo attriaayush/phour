cf3e8ba857afa6762028aea5dbbf9d20421209c0
LilUziCert <attriaayush@yahoo.com>
2020-05-03 16:14:09 +0100

    adding function literals

--- object/object.go
+++ object/object.go
@@ -1,6 +1,11 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"phour/ast"
+	"strings"
+)
 
 const (
 	INTEGER_OBJ      = "INTEGER"
@@ -8,6 +13,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 // NewEnvironment ..
@@ -55,6 +61,13 @@ type Environment struct {
 	store map[string]Object
 }
 
+// Function ..
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
 // Type for Integer
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 
@@ -95,4 +108,25 @@ func (e *Environment) Get(name string) (Object, bool) {
 func (e *Environment) Set(name string, val Object) Object {
 	e.store[name] = val
 	return val
+}
+
+// Type ..
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+
+// Inspect ..
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
 }
